7c4c5f6a7cc6ea8039367b9e61cf0432aff33a2c
Yosiris Mariñez <yosiris0510@gmail.com>
2021-11-16 10:41:08 +0100

    refactor test to add all number

--- add-all-numbers/my-solutions/forEach.add-all-numbers.spec.ts
+++ add-all-numbers/my-solutions/forEach.add-all-numbers.spec.ts
@@ -2,11 +2,11 @@ import { getTotalSum } from './for.add-all-numbers'
 
 describe('getTotalSum', () => {
   it('should get numbers sum', () => {
-    const total = [2, 3]
+    const total = [50, 3, 50]
 
     const result = getTotalSum(total)
 
-    const expected = 5
+    const expected = 103
     expect(result).toBe(expected)
   })
 })
--- add-all-numbers/my-solutions/reduce.add-all-number.spec.ts
+++ add-all-numbers/my-solutions/reduce.add-all-number.spec.ts
@@ -2,11 +2,11 @@ import { getTotalSum } from './for.add-all-numbers'
 
 describe('getTotalSum', () => {
   it('should get numbers sum', () => {
-    const total = [2, 3]
+    const total = [1, 3, 3]
 
     const result = getTotalSum(total)
 
-    const expected = 5
+    const expected = 7
     expect(result).toBe(expected)
   })
 })
