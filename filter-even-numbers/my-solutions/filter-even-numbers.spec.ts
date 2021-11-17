3ba3e602e9260a551f25920a453e02504ad54c11
Yosiris Mariñez <yosiris0510@gmail.com>
2021-11-17 11:23:23 +0100

    add kata test filter even number

---
+++ filter-even-numbers/my-solutions/filter-even-numbers.spec.ts
@@ -0,0 +1,11 @@
+import { filterNumberList } from './filter-even-numbers'
+
+describe('filterNumberList', () => {
+  it('filter the numbers even numbers from the matrix.', () => {
+    const numbers = [1, 2, 3, 4]
+
+    const result = filterNumberList(numbers)
+
+    expect(result).toEqual([2, 4])
+  })
+})
